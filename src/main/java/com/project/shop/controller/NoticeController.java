e8a093a429a57def1509fee0a9b9834e09130df6
Shin-dong-eun <tlsehddms1234@naver.com>
2023-09-06 11:00:32 +0900

    주문관리,공지사항,상품/수정삭제 페이지 구현

---
+++ src/main/java/com/project/shop/controller/NoticeController.java
@@ -0,0 +1,12 @@
+package com.project.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NoticeController {
+   @GetMapping(value = "/notice") 
+   public String notice() {
+	   return "notice/notice";
+   }
+}
---
+++ src/main/java/com/project/shop/controller/admin/adminMemberController.java
@@ -0,0 +1,13 @@
+package com.project.shop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class adminMemberController {
+
+	@GetMapping(value = "/admin/member")
+	   public String adminItemsList() {
+		   return "admin/admin_member";
+	   }
+}
