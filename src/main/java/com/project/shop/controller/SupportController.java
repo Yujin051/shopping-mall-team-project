6ff28ec6040d7652d10d1ed33b3951c76af7a09c
Yujin051 <sofi04080@gmail.com>
2023-09-06 14:22:27 +0900

    support controller/faq view update to tree

---
+++ src/main/java/com/project/shop/controller/SupportController.java
@@ -0,0 +1,23 @@
+package com.project.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/support")
+@Controller
+public class SupportController {
+	
+	@GetMapping(value = "direction")
+	public String direction() {
+		return "/support/direction";
+	}
+	
+	@GetMapping(value = "faq")
+	public String faq() {
+	    return "/support/faq";
+	}
+}
