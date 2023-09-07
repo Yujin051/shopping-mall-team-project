855125ffe9e287fad9776b3ba0bc34969d2fece7
Yujin051 <142381868+Yujin051@users.noreply.github.com>
2023-09-07 09:46:43 +0900

    Update AdminItemsController.java
    merge AdminItemsController.java

--- src/main/java/com/project/shop/controller/admin/AdminItemsController.java
+++ src/main/java/com/project/shop/controller/admin/AdminItemsController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminItemsController {
 
 	@GetMapping(value = "/admin/ItemList")
-	   public String adminItemsList() {
-		   return "admin/admin_ItemList";
-	   }
+	public String adminItemsList() {
+		return "admin/admin_ItemList";
+	}
+	@GetMapping("/admin/newItem")	
+	public String newItem() {
+		return "/admin/newItem";
+	}
+	
+	@GetMapping("/admin/modifyItem")
+	public String modifyItem() {
+		return "/admin/modifyItem";
+	}
 }
