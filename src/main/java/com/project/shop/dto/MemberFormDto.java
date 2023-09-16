d0c5a43bf78c95a0f03f250bb644bef8315a07e1
TrioL1 <losma2621@gmail.com>
2023-09-16 15:45:15 +0900

    최종

--- src/main/java/com/project/shop/dto/MemberFormDto.java
+++ src/main/java/com/project/shop/dto/MemberFormDto.java
@@ -25,6 +25,7 @@ public class MemberFormDto {
 	@Length(min = 4, max = 16, message = "비밀번호는 4자 이상 16자 이하로 입력해주세요")
 	private String password;
 	
+	@Length(min = 11, max = 12, message = "정확한 전화번호를 입력해 주세요")
 	private String phonenum;
 
 	private LocalDate date;
--- src/main/java/com/project/shop/entity/Notice.java
+++ src/main/java/com/project/shop/entity/Notice.java
@@ -3,6 +3,7 @@ package com.project.shop.entity;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
@@ -36,6 +37,7 @@ public class Notice {
 	
 	@Column(name = "notice_date")
 	@CreatedDate
+	@LastModifiedDate
 	private LocalDate date;
 	
 	@Builder
