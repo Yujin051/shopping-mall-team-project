e0dc2060bfe23f805fc6d242d1597559802a1eee
ynkim99 <rladudska99@naver.com>
2023-09-18 15:20:05 +0900

    충동해결

--- src/main/java/com/project/shop/repository/OrderRepository.java
+++ src/main/java/com/project/shop/repository/OrderRepository.java
@@ -23,4 +23,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select new com.project.shop.dto.OrdersManageDto(i.id, o.orderStatus, o.orderDate, i.itemName, o.orderPrice, i.imgSaved) from Order o join o.item i join o.member m where m.id = :id")
     List<OrdersManageDto> findOrderMyManageDto(Long id);
+
+    @Query("select new com.project.shop.dto.OrdersManageDto(o.id, i.id, o.orderCount, o.orderPrice, i.itemName, o.orderDate, m.email, o.orderStatus, i.imgSaved) from Order o join o.item i join o.member m")
+    Page<OrdersManageDto> findOrdersManageDto(Pageable pageable);
 }
