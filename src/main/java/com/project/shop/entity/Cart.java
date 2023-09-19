72797c50ae6822d68979ff468bab5a38353af80b
ynkim99 <rladudska99@naver.com>
2023-09-19 19:05:57 +0900

    7시 5분 합치기

--- src/main/java/com/project/shop/entity/Cart.java
+++ src/main/java/com/project/shop/entity/Cart.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Cart {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "cart_id")
     private Long id;
 
